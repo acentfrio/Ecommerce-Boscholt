9708fbeb096ba5bec1f1a147643440a525797db3
Alexander R <acentfrio@gmail.com>
2024-08-14 16:49:33 +0200

    updates vars deploy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -39,6 +39,7 @@ export default async function Home() {
               </div>
 
               {session?.user && <LatestPost />}
+              <div />
             </div>
           </div>
         </div>
